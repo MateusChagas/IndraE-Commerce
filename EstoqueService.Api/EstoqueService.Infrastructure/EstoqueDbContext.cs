d08ea498c98dfbe752cf2832fbb6874411becd75
Mateus Oliveira <mateuschagasoliveira@gmail.com>
2025-09-13 19:10:15 -0300

    Finalização projeto Paulo

--- EstoqueService.Api/EstoqueService.Infrastructure/EstoqueDbContext.cs
+++ EstoqueService.Api/EstoqueService.Infrastructure/EstoqueDbContext.cs
@@ -8,4 +8,17 @@ public class EstoqueDbContext:DbContext
     {
     }
     public DbSet<Domain.Produto> Produtos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Domain.Produto>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Nome).IsRequired().HasMaxLength(100);
+            entity.Property(e => e.Descricao).HasMaxLength(500);
+            entity.Property(e => e.Quantidade).IsRequired();
+            entity.Property(e => e.Preco).IsRequired().HasColumnType("decimal(18,2)");
+        });
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,25 +1,82 @@
+using EstoqueService.Infrastructure;
+using MassTransit;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Serilog;
+using Serilog.Events;
+using Serilog.Extensions.Logging;
+using Serilog.AspNetCore;
+using System.Text;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+// Configurar Serilog
+Log.Logger = new LoggerConfiguration()
+.WriteTo.Console()
+.WriteTo.File("logs/inventory-.txt", rollingInterval: RollingInterval.Day)
+.CreateLogger();
+
+builder.Host.UseSerilog();
+
+// Configurar EF Core
+builder.Services.AddDbContext<EstoqueDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+// Configurar MassTransit + RabbitMQ
+builder.Services.AddMassTransit(x =>
+{
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host("rabbitmq", "/", h =>
+        {
+            h.Username("guest");
+            h.Password("guest");
+        });
+    });
+});
+
+
+// Autenticação JWT
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer(options =>
+{
+  
+    var jwtKey = builder.Configuration["Jwt:Key"];
+    if (string.IsNullOrEmpty(jwtKey))
+    {
+        throw new InvalidOperationException("A chave JWT não está configurada. Verifique a configuração 'Jwt:Key'.");
+    }
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+    };
+});
+
+
+builder.Services.AddAuthorization();
+
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
-app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
+
 app.MapControllers();
+
 
 app.Run();
